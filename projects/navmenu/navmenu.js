db3ff189d763e61219a269d5386a45ae17b7e375
mukulchakravarthi <mchakrav@risd.edu>
2018-02-05 23:57:58 -0500

    returned all

---
+++ projects/navmenu/navmenu.js
@@ -0,0 +1,1 @@
+$( '#nav li:has(ul)' ).doubleTapToGo();
---
+++ projects/timeline/mythology.js
@@ -0,0 +1,77 @@
+window.onload = function () {
+  startSetTimeoutAnimation();
+  startAnimFrameAnimation();
+  startSetTimeout2Animation();
+  startAnimFrame2Animation();
+};
+
+function startSetTimeoutAnimation() {
+  var refreshRate = 1000 / 60;
+  var maxXPosition = 800;
+  var rect = document.getElementById('rect0');
+  var speedX = 6;
+  var positionX = 0;
+
+  window.setInterval(function () {
+    positionX = positionX + speedX;
+    if (positionX > maxXPosition || positionX < 0) {
+      speedX = speedX * -1;
+    }
+    rect.style.left = positionX + 'px';
+  }, refreshRate);
+}
+
+function startAnimFrameAnimation() {
+  var refreshRate = 1000 / 60;
+  var maxXPosition = 800;
+  var rect = document.getElementById('rect1');
+  var speedX = 5;
+  var positionX = 0;
+
+  function step() {
+    positionX = positionX + speedX;
+    if (positionX > maxXPosition || positionX < 0) {
+      speedX = speedX * -1;
+    }
+    rect.style.left = positionX + 'px';
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+}
+
+
+function startSetTimeout2Animation() {
+  var refreshRate = 1000 / 60;
+  var maxXPosition = 800;
+  var rect = document.getElementById('rect2');
+  var speedX = 4;
+  var positionX = 0;
+
+  window.setInterval(function () {
+    positionX = positionX + speedX;
+    if (positionX > maxXPosition || positionX < 0) {
+      speedX = speedX * -1;
+    }
+    rect.style.left = positionX + 'px';
+  }, refreshRate);
+}
+
+function startAnimFrame2Animation() {
+  var refreshRate = 1000 / 60;
+  var maxXPosition = 800;
+  var rect = document.getElementById('rect3');
+  var speedX = 3;
+  var positionX = 0;
+
+  function step() {
+    positionX = positionX + speedX;
+    if (positionX > maxXPosition || positionX < 0) {
+      speedX = speedX * -1;
+    }
+    rect.style.left = positionX + 'px';
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+}
